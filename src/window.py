718cc7f378cc9d28da7bc488fded44a66926ea2e
J4nk0wski <jankowskik2006@gmail.com>
2026-05-05 20:43:07 +0200

    Dodanie funckji wyswietlania obiektu Rect, odswiezania ekranu oraz wypelniania go kolorem

--- src/window.py
+++ src/window.py
@@ -1,17 +1,33 @@
 import pygame
- 
-COLOR_BACKGROUND = (30, 30, 30)    # ciemnoszare tło
-COLOR_DRONE      = (100, 180, 255) # niebieski dron
- 
- 
-class Renderer:
- 
-    def __init__(self, screen):
-        self.screen = screen
- 
-    def draw(self, drone_x, drone_y, drone_size=40):
-        # 1. Tło
-        self.screen.fill(COLOR_BACKGROUND)
- 
-        # 2. Dron jako kwadrat
-        pygame.draw.rect(self.screen, COLOR_DRONE, (drone_x, drone_y, drone_size, drone_size))
+#nalezy dodac komentarze
+class Window:
+
+#ustawia domyslny rozmiar okna na 1280x720 i nazwe gry na GAME 
+    def __init__(self, size=(1280,720), game_name="Game"):
+        self.screen = pygame.display.set_mode(size)
+        self.color = (255, 255, 255)
+        self.rectangles = []
+#inicjalizyje okno przy tworzeniu obiektu Window
+        pygame.display.set_caption(game_name)
+    
+    def change_background_color(self, new_color=(255, 255, 255)):
+        self.color = new_color
+    
+    def update(self):
+        pygame.display.update()
+
+    def add_rectangle(self, rectangle):
+        self.rectangles.append(rectangle)
+    
+    def draw_rect(self, rectangle):
+        pygame.draw.rect(self.screen, (255, 0, 0), rectangle)
+
+    def render(self):
+        self.screen.fill(self.color)
+        for rectangle in self.rectangles:
+            self.draw_rect(rectangle)
+
+        self.update()
+
+        
+    
