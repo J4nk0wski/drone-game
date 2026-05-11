12838b30bcd4705f32b6ad54e7638c9e6cb9ad92
MJagiel <mjagiel111@gmail.com>
2026-05-11 20:53:21 +0200

    Dodanie game_logic.py

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,9 +1,9 @@
-from .shared import GameObject, CollisionSide, CollisionInfo
+from shared import GameObject, CollisionSide, CollisionInfo
 
-from .drone import Drone
+from drone import Drone
 
 """
-
+Sprawdz czy wystąpiła kolizja i oblicza dane kolizji CollisionInfo
 """
 def check_collision(dynamic: GameObject, static: GameObject) -> CollisionInfo:
     if not dynamic.rect.colliderect(static.rect):
@@ -22,4 +22,3 @@ def check_collision(dynamic: GameObject, static: GameObject) -> CollisionInfo:
     if isinstance(dynamic, Drone):
         return CollisionInfo(True, side, static, dynamic.velocity)
     return CollisionInfo(True, side, static)
-
