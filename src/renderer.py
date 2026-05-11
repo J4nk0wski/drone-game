75de38b0b3d7f501ce7260f6ab16dcfc2ddf9c09
Krzysztof Kamiński <krzysztofkaminski2005@gmain.com>
2026-05-11 23:46:57 +0200

    dodano funkcję do wypisywania Game Over

--- src/renderer.py
+++ src/renderer.py
@@ -1,4 +1,13 @@
 import pygame
 
-def end_screen(screen):
-    pass
+from src.window import Window
+
+"""
+wypisuje na środku ekranu napis "Game Over"
+"""
+def end_screen_text(screen: Window) -> None:
+    font = pygame.font.SysFont("monospace", 50)
+    text = font.render("Game Over", True, (255, 255, 255))
+    text_rect = text.get_rect()
+    text_rect.center = screen.screen_size[0] / 2, screen.screen_size[1] / 2
+    screen.screen.blit(text, text_rect)
