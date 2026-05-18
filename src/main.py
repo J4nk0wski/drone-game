6161766051515e4de78ede6772c69a308ac04958
Dawid Kawalek <dawidkawalek1@gmail.com>
2026-05-18 21:42:20 +0200

    "Podpięcie wyciętej grafiki dron.png i wprowadzenie dynamicznych ścieżek przez moduł os"

--- src/main.py
+++ src/main.py
@@ -1,106 +1,102 @@
 import pygame
-from drone import Drone
-from shared import GameObject, ObjectType, CollisionSide
-from game_logic import check_collision
-from window import Window, Camera
+import os
+from src.drone import Drone
+from src.shared import GameObject, ObjectType, Color
+from src.game_logic import check_collision
+# ZMIANA 1: Importujemy klasę Camera z window.py
+from src.window import Window, Camera
 
 
 def main():
     pygame.init()
 
-    # Tworzenie okna
     game_window = Window(size=(800, 600), game_name="Drone Game")
-    game_window.change_background_color((0, 0, 0))
+    game_window.change_background_color(Color.BG_NIGHT)
 
-    # Używamy argumentów pozycyjnych dla kamery (rozwiązuje błąd TypeError)
-    camera = Camera(800, 600, 15.0, 0.08)
+    # ZMIANA 2: Tworzymy kamerę
+    camera = Camera(screen_width=800, screen_height=600)
 
     clock = pygame.time.Clock()
     running = True
 
-    # Inicjalizacja drona
     player_drone = Drone(x=370, y=50, width=60, height=40)
+    folder_skryptu = os.path.dirname(__file__)  # to da nam ścieżkę do folderu 'src'
+    sciezka_drona = os.path.join(folder_skryptu, "dron.png")
+
+    player_drone.create_image(sciezka_drona)
     player_drone.gravity = 0.5
 
-    # Inicjalizacja podłogi
-    floor = GameObject(x=0, y=550, width=800, height=50, object_type=ObjectType.FLOOR)
+    # ZMIANA 3: Poszerzamy podłogę (od -2000 do szerokości 4000), bo teraz dron może daleko odlecieć!
+    floor = GameObject(x=-2000, y=550, width=4000, height=50, object_type=ObjectType.FLOOR)
 
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
-        left_power = 0.0
-        right_power = 0.0
-
-        # --- Sterowanie ---
+        # --- STEROWANIE ---
         keys = pygame.key.get_pressed()
+
+        # ZMIANA 4: Domyślna moc silników (gdy puszczasz klawisze)
+        left_power, right_power = 0.0, 0.0
 
         if keys[pygame.K_UP]:
             player_drone.velocity.y -= 1.0
             left_power, right_power = 0.8, 0.8
 
         if keys[pygame.K_LEFT]:
-            player_drone.velocity.x = -5
+            player_drone.velocity.x -= 0.5
             left_power, right_power = 0.2, 1.0
         elif keys[pygame.K_RIGHT]:
-            player_drone.velocity.x = 5
+            player_drone.velocity.x += 0.5
             left_power, right_power = 1.0, 0.2
         else:
-            player_drone.velocity.x = 0
+            player_drone.velocity.x *= 0.95
 
-        # --- Fizyka ---
+        player_drone.velocity.x = max(-8.0, min(8.0, player_drone.velocity.x))
+        player_drone.velocity.y = max(-10.0, player_drone.velocity.y)
+
+        # --- FIZYKA GRY ---
         player_drone.velocity.y += player_drone.gravity
-        player_drone.y += player_drone.velocity.y
-        player_drone.x += player_drone.velocity.x
 
-        # Synchronizacja rectów przed kolizją
+        # ZMIANA 5: Tutaj MUSZĄ być plusy, żeby sterowanie zgadzało się z kierunkiem
+        player_drone.y += player_drone.velocity.y
         player_drone.rect.y = player_drone.y
+
+        player_drone.x += player_drone.velocity.x
         player_drone.rect.x = player_drone.x
 
-        # Aktualizacja kamery
+        # --- KOLIZJE ---
+        collision_info = check_collision(player_drone, floor)
+        if collision_info.collision:
+            player_drone.velocity.y = 0
+            player_drone.y = floor.top - player_drone.height
+            player_drone.rect.y = player_drone.y
+
+        # --- AKTUALIZACJA KAMERY ---
+        # ZMIANA 6: Kamera śledzi pozycję drona
         camera.update(player_drone)
 
-        # --- Kolizje ---
-        collision_info = check_collision(player_drone, floor)
-        if collision_info and collision_info.collision:
-            side = collision_info.side
-
-            if side == CollisionSide.TOP:
-                player_drone.velocity.y = 0
-                player_drone.y = floor.rect.top - player_drone.height
-            elif side == CollisionSide.BOTTOM:
-                player_drone.velocity.y = 0
-                player_drone.y = floor.rect.bottom
-            elif side == CollisionSide.LEFT:
-                player_drone.velocity.x = 0
-                player_drone.x = floor.rect.left - player_drone.width
-            elif side == CollisionSide.RIGHT:
-                player_drone.velocity.x = 0
-                player_drone.x = floor.rect.right
-
-            # Aktualizacja pozycji po poprawce kolizji
-            player_drone.rect.y = player_drone.y
-            player_drone.rect.x = player_drone.x
-
-        # --- Renderowanie ---
+        # --- RYSOWANIE NA EKRANIE ---
         if game_window.background_image:
             game_window.screen.blit(game_window.background_image, (0, 0))
         else:
             game_window.screen.fill(game_window.color)
 
-        # Rysowanie z uwzględnieniem kamery
+        # ZMIANA 7: Przepuszczamy obiekty przez kamerę (apply), żeby narysować je z przesunięciem
         floor_rect_cam = camera.apply(floor)
+        pygame.draw.rect(game_window.screen, Color.FOREST_GREEN, floor_rect_cam)
+
         drone_rect_cam = camera.apply(player_drone)
-
-        pygame.draw.rect(game_window.screen, (100, 100, 100), floor_rect_cam)
-
         if player_drone.img:
-            game_window.screen.blit(player_drone.img, drone_rect_cam)
+            # Skalujemy grafikę na wypadek użycia w przyszłości opcji 'zoom' z kamery
+            scaled_img = pygame.transform.scale(player_drone.img,
+                                                (int(drone_rect_cam.width), int(drone_rect_cam.height)))
+            game_window.screen.blit(scaled_img, drone_rect_cam)
         else:
-            pygame.draw.rect(game_window.screen, (255, 0, 0), drone_rect_cam)
+            pygame.draw.rect(game_window.screen, Color.RED, drone_rect_cam)
 
-        # Rysujemy tylko paski mocy (usuwamy niedziałającą telemetrię)
+        # ZMIANA 8: Rysujemy paski mocy silników (interfejs zostaje w miejscu, nie podlega kamerze)
         game_window.draw_engine_power(left_power, right_power)
 
         game_window.update()
