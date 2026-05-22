daae2c127082559c66ed2beedf4ea1ad44762868
Krzysztof Kaminski <krzysztofkaminski2005@gmail.com>
2026-05-22 23:48:40 +0200

    poprawa wydajności w check_collision

--- src/game_logic.py
+++ src/game_logic.py
@@ -1,17 +1,15 @@
 from shared import GameObject, CollisionSide, CollisionInfo
-from shared import Vector2
-from drone import Drone
 
 """
 Sprawdz czy wystąpiła kolizja i oblicza dane kolizji CollisionInfo
 """
 def check_collision(dynamic: GameObject, static: GameObject) -> CollisionInfo:
-    if not dynamic.rect.colliderect(static.rect):
-        return CollisionInfo(False, CollisionSide.NONE, None, None)
-
     d_rect = dynamic.rect
     s_rect = static.rect
-    
+
+    if not d_rect.colliderect(s_rect):
+        return CollisionInfo(False, CollisionSide.NONE, None, None)
+
     overlaps = {
         CollisionSide.TOP: d_rect.bottom - s_rect.top,
         CollisionSide.BOTTOM: s_rect.bottom - d_rect.top,
@@ -19,6 +17,6 @@ def check_collision(dynamic: GameObject, static: GameObject) -> CollisionInfo:
         CollisionSide.RIGHT: s_rect.right - d_rect.left,
     }
     side = min(overlaps, key=overlaps.get)
-    if isinstance(dynamic, Drone):
-        return CollisionInfo(True, side, static, dynamic.velocity)
-    return CollisionInfo(True, side, static, None)
+    velocity_to_return = getattr(dynamic, 'velocity', None)
+
+    return CollisionInfo(True, side, static, velocity_to_return)
